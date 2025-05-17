207c3bda86e99637f8230090f4fbfe1af930bfbb
FranciscoSoares0 <fsoares751@gmail.com>
2025-05-17 13:34:22 +0100

    remove env

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    API_BASE_URL: 'http://localhost:3000',
-    tmdbAPIURL: 'https://api.themoviedb.org/3',
-    tmdbAPIKey: '8088648ba900145c02c1260ec4f598b1'
-};
